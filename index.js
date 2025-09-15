c370d79fe5c8d9550ecf805fe1c194b89c5f65ea
kego07 <131258727+bantu46@users.noreply.github.com>
2025-09-15 12:57:20 -0400

    Created a function that returns a random computer choice

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+let humanScore = 0;
+let computerScore = 0;
+
+// A FUNCTION THAT RETURNS COMPUTER CHOICE
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);
+   
+    if (choice === 0){
+        return "rock";
+    } else if (choice === 1){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
