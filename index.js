9e2232a4aab7b24cdd211335bcc02625e2bd83aa
kego07 <131258727+bantu46@users.noreply.github.com>
2025-09-18 18:02:47 -0400

    Added more functions to the game.
    Improved the playRound function to increment the round winner's score and log a winner announcement.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let humanScore = 0;
 let computerScore = 0;
 let input = prompt("Choose Rock, Paper, or Scissors", "User Choice");
-let humanChoice = getHumanChoice();
 let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+let result = checkWinner(humanChoice, computerChoice);
 
-// THE LOGIC TO GET THE HUMAN CHOICE
-// SET THE HUMAN CHOICE TO LOWER CASE 
+// A FUNCTION THAT GETS THE PLAYER INPUT
 
 function getHumanChoice(){
     return input.toLowerCase();
 }
 
-// A FUNCTION THAT RETURNS COMPUTER CHOICE
+// A FUNCTION THAT RETURNS A RANDOM COMPUTER CHOICE
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
@@ -25,35 +25,55 @@ function getComputerChoice(){
     }   
 }
 
-// // LOGIC TO DETERMINE THE WINNER OF ONE ROUND
+// LOGIC TO DETERMINE THE WINNER OF ONE ROUND, 
+// AND UPDATES THE SCORE OF THE WINNER OF THE ROUND
 
 function checkWinner(humanChoice, computerChoice){
-
+    
     if (humanChoice === computerChoice){
         return "Tie";
     } else if (
         (humanChoice === "rock" && computerChoice === "scissors") || 
         (humanChoice === "paper" && computerChoice === "rock") || 
         (humanChoice === "scissors" && computerChoice === "paper")
-    ){   
-        return "Player Wins";
+    ){  
+        humanScore++; 
+        return "Player";
     } else {
-
-        return "Computer Wins";
+        computerScore++;
+        return "Computer";
     } 
 }
 
 
+// FUNCTION THAT DISPLAYS A MESSAGE OF THE WINNER
+
+function displayWinner(winner, humanChoice, computerChoice){
+    if (winner === "Tie"){
+        console.log("It is a tie.")
+    } else if (winner === "Player"){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+    }
+}
+
 // A FUNCTION THAT PLAYS A SINGLE ROUND
 
 function playRound(humanChoice, computerChoice){
+    
     console.log("Player choice: " + humanChoice);
     console.log("Computer choice: " + computerChoice);
-    console.log(checkWinner(humanChoice, computerChoice));
+    displayWinner(result, humanChoice, computerChoice);
+    console.log("Scores:");
+    console.log("Player: " + humanScore);
+    console.log("Computer: " + computerScore);
 }
 
 function game(){
+   
     playRound(humanChoice, computerChoice);
+
 }
 
 game();
